33c283a30618bf81d22b6ee1ed7ec3e8f06b8bf9
Ornery116 <2128190795@qq.com>
2026-07-26 22:57:16 +0800

    Initial commit

---
+++ src/main/java/ac/test/PluginLoader.java
@@ -0,0 +1,17 @@
+package ac.test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginLoader extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
